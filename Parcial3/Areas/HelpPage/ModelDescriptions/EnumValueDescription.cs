b6563264f134765ac853d02d1c26d38d0099b3d4
SebastianVahos <vahossebas@gmail.com>
2025-04-23 16:15:43 -0500

    Agregar archivos de proyecto.

---
+++ Parcial3/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Parcial3.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
